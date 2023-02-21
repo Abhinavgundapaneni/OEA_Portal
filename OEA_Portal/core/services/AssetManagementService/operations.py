be044f27fcc2c90bdb2dd250cc64091f39a5589d
Abhinav Gundapaneni <agundapaneni@microsoft.com>
2023-02-21 17:45:35 +0530

    as

--- OEA_Portal/core/services/AssetManagementService/operations.py
+++ OEA_Portal/core/services/AssetManagementService/operations.py
@@ -56,7 +56,7 @@ def parse_deployment_template_and_install_artifacts(file_path:str, azure_client:
         template_str = f.read()
     parameters = template_json["parameters"]
     sms = SynapseManagementService(azure_client, 'rg-oea-abhinav4')
-    for param in parameters.keys()[1:]:
+    for param in list(parameters.keys())[1:]:
         template_str = template_str.replace(f"[parameters('{param}')]")
     template_json = json.loads(template_str)
     datasets = [resource for resource in template_json["resources"] if resource["type"] == "Microsoft.Synapse/workspaces/datasets" ]
